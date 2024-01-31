ebd4f0c36eaff7ff220ccbab5921e15a4d6c8938
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2024-01-31 15:34:40 -0500

    Initial commits to get the store going with categories

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+        <div className="py-10 mx-auto">
+            <p className="text-center text-black text-xs">
+                &copy; 2024 ABCDOLLARSTORE All rights reserved.
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
