86672b2f42f480e73af64c1254be543f25910e3d
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2024-02-01 02:19:11 -0500

    Remove from cart works and cart page is set up

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -3,6 +3,7 @@
 import Container from "@/components/ui/container"
 import useCart from "@/hooks/use-cart"
 import { useEffect, useState } from "react"
+import CartItem from "./components/cart-item"
 
 const CartPage = () => {
     const [isMounted,setIsMounted] = useState(false)
@@ -20,7 +21,16 @@ const CartPage = () => {
         <Container>
             <div className="px-4 py-16 sm:px-6 lg:px-8">
                 <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
-
+                <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+                    <div className="lg:col-span-7">
+                        {cart.items.length === 0 && <p className="text-neutral-500">No Items Added To Cart</p>}
+                        <ul>
+                            {cart.items.map((item)=> (
+                                <CartItem key={item.id} data={item}/>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </Container>
     </div>
